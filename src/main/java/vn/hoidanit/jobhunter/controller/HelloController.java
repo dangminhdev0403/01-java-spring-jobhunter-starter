5aa90e385f7663b36acaf7e58cc8e99865e4bed2
Your Name <minhphp2005@gmail.com>
2024-12-20 03:28:37 +0700

    the first api

--- src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/HelloController.java
@@ -3,11 +3,29 @@ package vn.hoidanit.jobhunter.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
 @RestController
 public class HelloController {
+    private final UserService userService;
+
+    public HelloController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
     public String getHelloWorld() {
         return "Hello World (Hỏi Dân IT & Eric)";
+    }
+
+    @GetMapping("/user/create")
+    public String createUser() {
+        User user = new User();
+        user.setName("Hỏi Dân IT");
+        user.setEmail("devjadoj@ozfud.tp");
+        user.setPassword("123456");
+        this.userService.saveUser(user);
+        return "ok";
     }
 }
